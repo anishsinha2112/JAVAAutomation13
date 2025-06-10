bf9554a5558309da8d2f5a3bb2c21bf2cb1f34cd
anishsinha2112 <158539935+anishsinha2112@users.noreply.github.com>
2025-06-10 23:25:42 +0530

    New update

---
+++ src/ex_01_Java_Basic/Nine_table.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Nine_table {
+    public static void main(String[] args) {
+        System.out.println("Multiplication Table of 9:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("9 x %2d = %2d%n", i, 9 * i);
+        }
+    }
+
+
+}
