af4509e29b1311b6bf765eb8607326b82e3c1da0
anishsinha2112 <158539935+anishsinha2112@users.noreply.github.com>
2025-06-24 06:11:02 +0530

    New update

---
+++ src/ex_14_Strings/InterviewCoding.java
@@ -0,0 +1,18 @@
+package ex_14_Strings;
+
+import java.util.Scanner;
+
+public class InterviewCoding {
+    public static void main(String[] args) {
+        //  Palindrome
+        //  madam ->  reverse(madam) -> madam ==
+        //  s1 = madam
+        // s1_reverse = madam
+        //  s1 == s1_reverse
+        // G
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
