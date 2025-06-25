8e5f3ba0667a4c5a23c8abb33789df887e4ee842
anishsinha2112 <158539935+anishsinha2112@users.noreply.github.com>
2025-06-25 22:23:43 +0530

    New update 25 june

--- src/ex_16_Arrays/Lab157_Interview_Q_2nd_High_Number_Array.java
+++ src/ex_16_Arrays/Lab157_Interview_Q_2nd_High_Number_Array.java
@@ -11,5 +11,28 @@ public class Lab157_Interview_Q_2nd_High_Number_Array {
         System.out.println(numbers[numbers.length-2]);
         System.out.println(numbers[numbers.length-3]);
         // You find it without using the sort function.
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        int third = Integer.MIN_VALUE;
+
+        for (int num : numbers) {
+            if (num > first) {
+                // Update all
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                third = second;
+                second = num;
+            } else if (num > third && num != second && num != first) {
+                third = num;
+            }
+        }
+
+        System.out.println("First Largest: " + first);
+        System.out.println("Second Largest: " + second);
+        System.out.println("Third Largest: " + third);
     }
 }
+
